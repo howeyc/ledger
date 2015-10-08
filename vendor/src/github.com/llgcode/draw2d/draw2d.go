2b72530f4851868b4d58820da0b660161282e28c
Chris Howey <chris@howey.me>
2015-10-08 10:39:01 -0500

    Clean dependencies

--- vendor/src/github.com/llgcode/draw2d/draw2d.go
+++
@@ -1,5 +0,0 @@
-// Copyright 2010 The draw2d Authors. All rights reserved.
-// created: 13/12/2010 by Laurent Le Goff
-
-// Package draw2d provides a Graphic Context that can draw vector form on canvas.
-package draw2d
