2f424529c0eb217f09bd21473f34f8e2fab1f9df
Chris Howey <chris@howey.me>
2021-07-25 09:51:08 -0500

    format of transaction on man page

--- ledger/cmd/version.go
+++ ledger/cmd/version.go
@@ -11,7 +11,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Version of ledger",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Ledger v0.3.3")
+		fmt.Println("Ledger v0.3.4")
 	},
 }
 
