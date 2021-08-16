867b0e4b8698dba9aa4eb43f511b6f561b5ad11e
Chris Howey <chris@howey.me>
2021-08-16 17:40:08 -0500

    version from git tag

--- ledger/cmd/version.go
+++ ledger/cmd/version.go
@@ -2,8 +2,16 @@ package cmd
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/spf13/cobra"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+	builtBy = "user"
 )
 
 // versionCmd represents the version command
@@ -11,7 +19,13 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Version of ledger",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Ledger v0.3.4")
+		fmt.Printf("ledger %s\n", version)
+		fmt.Printf("- build/commit: %s\n", commit)
+		fmt.Printf("- build/date: %s\n", date)
+		fmt.Printf("- build/user: %s\n", builtBy)
+		fmt.Printf("- os/type: %s\n", runtime.GOOS)
+		fmt.Printf("- os/arch: %s\n", runtime.GOARCH)
+		fmt.Printf("- go/version: %s\n", runtime.Version())
 	},
 }
 
