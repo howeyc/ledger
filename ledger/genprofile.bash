5d0cd97a632b06e851b6adc9f7a338a6135a3a5f
Chris Howey <chris@howey.me>
2023-08-09 09:24:27 -0500

    perf: profile generation

--- ledger/genprofile.bash
+++ ledger/genprofile.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-./ledger --profile "bal.pprof" bal
-./ledger --profile "reg.pprof" reg
-./ledger --profile "print.pprof" print
+./ledger --profile "bal.pprof" bal > /dev/null
+./ledger --profile "reg.pprof" reg > /dev/null
+./ledger --profile "print.pprof" print > /dev/null
 
 rm default.pgo
 
