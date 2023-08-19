b106e7a221cdfcfaa43fb7a38cba5d997217c2f3
Chris Howey <chris@howey.me>
2023-08-19 13:03:41 -0500

    docs: show off performance

--- ledger/genperf.bash
+++ ledger/genperf.bash
@@ -5,3 +5,33 @@ cbench --export-markdown perf-bal.md "ledger bal" "./ledger bal" "hledger bal"
 cbench --export-markdown perf-reg.md "ledger reg" "./ledger reg" "hledger reg"
 cbench --export-markdown perf-print.md "ledger print" "./ledger print" "hledger print"
 
+echo "## Performance" > perf.md
+echo "" >> perf.md
+echo "Comparison between various ledger-like applications:" >> perf.md
+echo "" >> perf.md
+echo "- ledger-go" >> perf.md
+echo "- [ledger-cli](https://ledger-cli.org)" >> perf.md
+echo "- [hledger](https://hledger.org)" >> perf.md
+echo "" >> perf.md
+
+echo "### Stats" >> perf.md
+echo "" >> perf.md
+cat perf-stats.md | sed -e 's/\.\/ledger/ledger-go/g' | sed -e 's/ledger /ledger-cli /g' >> perf.md
+echo "" >> perf.md
+
+echo "### Balance" >> perf.md
+echo "" >> perf.md
+cat perf-bal.md | sed -e 's/\.\/ledger/ledger-go/g' | sed -e 's/ledger /ledger-cli /g' >> perf.md
+echo "" >> perf.md
+
+echo "### Register" >> perf.md
+echo "" >> perf.md
+cat perf-reg.md | sed -e 's/\.\/ledger/ledger-go/g' | sed -e 's/ledger /ledger-cli /g' >> perf.md
+echo "" >> perf.md
+
+echo "### Print" >> perf.md
+echo "" >> perf.md
+cat perf-print.md | sed -e 's/\.\/ledger/ledger-go/g' | sed -e 's/ledger /ledger-cli /g' >> perf.md
+echo "" >> perf.md
+
+rm perf-stats.md perf-bal.md perf-reg.md perf-print.md
